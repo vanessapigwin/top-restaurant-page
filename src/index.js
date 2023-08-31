b942df24d58ae11a5813e2d5347d7e239db6f303
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-08-31 17:00:50 +0800

    configure webpack for multiple src js files q

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('test file folders ok')
+import './style.css';
+
+const makeHeader = ()=> {
+        
+    const header = document.createElement('div');
+    const title = document.createElement('div');
+    const buttonTray = document.createElement('div');
+
+    header.classList.add('header');
+    buttonTray.classList.add('button-tray-hor');
+
+    title.textContent = 'Steve\'s Cottage';
+    
+    header.appendChild(title);
+    
+    return header;
+};
+
+(() => {
+    const header = makeHeader()
+    document.body.appendChild(header);
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        home: './src/home.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'TOP Restaurant - Steve\'s Cottage',
+        }),
+      ],
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 }
