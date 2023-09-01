72c3895eac26f9eab9b34bc1372b23115c2659f4
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-09-01 18:33:29 +0800

    header and navigation ok, added readme

---
+++ contact.bundle.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunktop_restaurant_page"] = self["webpackChunktop_restaurant_page"] || []).push([["contact"],{
+
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   makeContact: () => (/* binding */ makeContact)
+/* harmony export */ });
+const makeContact = () => {
+    const contactDiv = document.createElement('div');
+    contactDiv.textContent = 'Hello from COntact';
+
+    return contactDiv
+}
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/contact.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udGFjdC5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9wLXJlc3RhdXJhbnQtcGFnZS8uL3NyYy9jb250YWN0LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IG1ha2VDb250YWN0ID0gKCkgPT4ge1xuICAgIGNvbnN0IGNvbnRhY3REaXYgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcbiAgICBjb250YWN0RGl2LnRleHRDb250ZW50ID0gJ0hlbGxvIGZyb20gQ09udGFjdCc7XG5cbiAgICByZXR1cm4gY29udGFjdERpdlxufVxuXG5leHBvcnQge21ha2VDb250YWN0fSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ menu.bundle.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunktop_restaurant_page"] = self["webpackChunktop_restaurant_page"] || []).push([["menu"],{
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   makeMenu: () => (/* binding */ makeMenu)
+/* harmony export */ });
+const makeMenu = () => {
+    const menuDiv = document.createElement('div');
+    menuDiv.textContent = 'Hello from Menu'
+
+    return menuDiv
+}
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/menu.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVudS5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9wLXJlc3RhdXJhbnQtcGFnZS8uL3NyYy9tZW51LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IG1ha2VNZW51ID0gKCkgPT4ge1xuICAgIGNvbnN0IG1lbnVEaXYgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcbiAgICBtZW51RGl2LnRleHRDb250ZW50ID0gJ0hlbGxvIGZyb20gTWVudSdcblxuICAgIHJldHVybiBtZW51RGl2XG59XG5cbmV4cG9ydCB7bWFrZU1lbnV9Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
