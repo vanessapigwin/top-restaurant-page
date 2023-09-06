818074bd92a2e164220282f0f931bfd72ab0eac0
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-09-06 14:22:05 +0800

    contact ok

--- README.md
+++ README.md
@@ -28,3 +28,4 @@ Lesson covering modules and webpack
 - Sword - <a target="_blank" href="https://icons8.com/icon/l4qWS2U4Ib7C/minecraft-sword">Minecraft Sword</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 - Diamond - <a target="_blank" href="https://icons8.com/icon/yJ3yOJg2Y3xH/minecraft-diamond">Minecraft Diamond</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 - Heart - <a target="_blank" href="https://icons8.com/icon/57696/pixel-heart">Pixel Heart</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+- Modern House - by SheepGG (https://www.youtube.com/watch?v=2cCYfw0RUfE)
--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,28 @@
+import cottage from './background_images/cabin.png';
+
 const makeContact = () => {
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
+    const imgDiv  = document.createElement('div');
+    const img = document.createElement('img');
+
+    p1.textContent = 'Your new culinary adventure awaits.'
+    p2.textContent = 'realsteve.notarickroll.com';
+    p3.textContent = '420 69 350';
+
+    img.classList.add('feature-img');
+    img.src = cottage;
+    imgDiv.classList.add('card-holder');
+    
+    imgDiv.appendChild(p1);
+    imgDiv.appendChild(p2);
+    imgDiv.appendChild(p3);
+    imgDiv.appendChild(img);
+    
     const contactDiv = document.createElement('div');
-    contactDiv.textContent = 'Hello from COntact';
+    contactDiv.classList.add('content', 'contact-content');
+    contactDiv.appendChild(imgDiv);
 
     return contactDiv
 }
